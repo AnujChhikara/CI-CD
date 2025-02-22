1ab2ffedb5d014b6d40aa8375f5078aefcf624ac
AnujChhikara <anujchhikara777@gmail.com>
2025-02-22 11:27:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from server home route");
+});
+app.listen(3000, () => {
+  "app is listening at port 3000";
+});
